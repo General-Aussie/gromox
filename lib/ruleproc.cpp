215ef4a271eec5e7e19003d83e1655a66f43c816
General Aussie <paghulor@gmail.com>
2023-10-26 20:16:14 +0100

    automatic process

--- lib/ruleproc.cpp
+++ lib/ruleproc.cpp
@@ -1045,13 +1045,13 @@ static ec_error_t process_meeting_requests(rxparam &par, const char* dir, int po
 			// // Get the start and end times from the content table
 			auto event_start_time = rows.pparray[i]->get<const uint64_t>(PR_START_DATE);
 			auto event_end_time = rows.pparray[i]->get<uint64_t>(PR_END_DATE);
-
+			mlog(LV_ERR, "W-PREC: checked %s", dir);
 			auto start_wholes = rop_util_nttime_to_unix(*event_start_time);
-			auto end_wholes = rop_util_nttime_to_unix(*event_end_time);
-			mlog(LV_ERR, "W-PREC: endwholes %u", *start_wholes);
+			// auto end_wholes = rop_util_nttime_to_unix(*event_end_time);
+			mlog(LV_ERR, "W-PREC: endwholes %u", start_wholes);
 
 			// Check for overlap with existing appointments
-			if ((&start_nttime >= start_wholes) && (&end_nttime <= end_wholes))
+			if ((&start_nttime >= rop_util_nttime_to_unix(*event_start_time)) && (&end_nttime <= rop_util_nttime_to_unix(*event_end_time)))
 			{
 				// Conflict found, set the status and return
 				mlog(LV_ERR, "W-PREC: conflict found %d", out_status);
