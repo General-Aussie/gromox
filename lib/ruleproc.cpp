769036ea3aa02343149b31d64eb048923dfd41e1
General Aussie <paghulor@gmail.com>
2023-10-28 17:45:59 +0100

    automatic process

--- lib/ruleproc.cpp
+++ lib/ruleproc.cpp
@@ -967,7 +967,8 @@ static ec_error_t get_policy_from_message_content(rxparam par, const char* dir){
 	int flags = 0;
 	bool isResource = false;
     if (par.ctnt->children.prcpts != nullptr) {
-		auto addrtype =  par.ctnt->children.prcpts->pparray.get<const char>(PR_ADDRTYPE);
+		auto addrtype =  par.ctnt->children.prcpts->pparray->get<const char>(PR_ADDRTYPE);
+		auto addretype =  par.ctnt->children.prcpts->pparray[]->get<const char>(PR_ADDRTYPE);
         if (addrtype != nullptr) {
             auto disptype = par.ctnt->children.prcpts->pparray.get<const uint32_t>(PR_DISPLAY_TYPE);
 			if (*disptype == static_cast<unsigned int>(DT_ROOM) || *disptype == static_cast<unsigned int>(DT_EQUIPMENT)){
