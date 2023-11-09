cb5e6a2363628e7e040895883e636b7a928bcb23
General Aussie <paghulor@gmail.com>
2023-11-09 09:34:38 +0100

    send response when meeting accepted -24

--- lib/ruleproc.cpp
+++ lib/ruleproc.cpp
@@ -1023,7 +1023,7 @@ static ec_error_t process_meeting_requests(rxparam par, const char* dir, bool *i
 						dst->proplist.set(PR_REPLY_REQUESTED, &tmp_byte) != 0)
 						return ecError;
 					
-
+					
 					subjectprefix = "Accepted";
 					if (dst->proplist.set(PROP_TAG(PT_LONG, propids.ppropid[1]), &responseAccepted) != 0)
 						return ecError;
@@ -1072,7 +1072,7 @@ static ec_error_t process_meeting_requests(rxparam par, const char* dir, bool *i
 					auto frm = par.ev_from;
 					ec_error_t e_result = ecRpcFailed;
 					ec_error_t pb_result = ecRpcFailed;
-					if(!exmdb_client::message_meeting_reply(dir, frm, par.ctnt, &pb_result)){
+					if(!exmdb_client::message_meeting_reply(dir, frm, dst, &pb_result)){
 						mlog(LV_DEBUG, "ruleproc: send_message failed");
 						return ecRpcFailed;
 					} else if (pb_result != ecSuccess) {
